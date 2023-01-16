5db37210073b79004dba3bcc68a11037ec33745d
deusExMac <tzagara@upatras.gr>
2023-01-17 00:14:50 +0200

    Added singleton pattern tha must be implemented.

--- applicationConfiguration.py
+++ applicationConfiguration.py
@@ -4,19 +4,60 @@ import os.path
 import argparse
 import configparser
 
+'''
+# Singleton class design
+
+class someClass:
+      loaded = False
+      def __init__(self, a=1, b=2):
+          if self.loaded:
+             return
+            
+          print('\t\tExecuting init in base class with', a, b)
+          
+          self.c1 = a
+          self.c2 = b
+          self.loaded = True
+          
+
+class singleton(someClass):
+
+      instance = None
+
+      def __new__(cls, a, b):
+        if cls.instance is None:
+            cls.instance = super().__new__(cls)
+            print('\tCreating instance')
+
+        print('\tReturning instance')
+        return cls.instance
+
+      def __init__(self, a=-1, b=-42):
+          print('\tCalling init in singleton with', a, b)
+          super().__init__(a, b)
+
+
+
+s1 = singleton(66, 77)
+#s1.a = 'sonia' 
+
+s2 = singleton(88, 99)
+#print(s2.a)
+#s2.a = -222
+
+s3 = singleton(110, 121)
+print(s1.c1)
+print(s1.c2)
+
+'''
+
 
 class applicationConfiguration:
 
-      # Singleton
-      # Also, implement this:
-      #   https://stackoverflow.com/questions/50804826/explain-python-singleton-class
-      #__shared_state = {}
+      # TODO: Singleton pattern here
       
       def __init__(self, argSpec=None, cfgFile=None, cmdArgs=None, configFileArgument='config', defaultConfigFile='default.conf'):
-
-          # Singleton
-          #self.__dict__ = self.__shared_state
-            
+   
           self.configFile = cfgFile
           self.passedArguments = cmdArgs
           self.argumentSpecification = argSpec
